9efc57847b0a9a4d6bd6a39ad14cd203a502dc61
Gouranga <gourangasaha1206@gmail.com>
2024-01-05 23:53:55 +0530

    Project

---
+++ Project 1/script.js
@@ -0,0 +1,9 @@
+let button = document.querySelector("#btn");
+let box = document.getElementById("box");
+
+button.addEventListener("click", ()=>{
+
+    box.style.display = (box.style.display ="none") ? "block" : "none";
+    
+}
+)
